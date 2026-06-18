553a0982321a1cca1b5c253ccfbc785a427ce555
Arthur Gomes <arthurgcunha2@gmail.com>
2026-06-18 10:12:45 -0300

    Função para resetar o jogo

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let turno=document.getElementById("status")
 let JogadorAtual="X"
 let casa=document.querySelectorAll(".celula")
 
+let botao_reiniciar=document.getElementById("btn-reiniciar")
+botao_reiniciar.addEventListener("click",ReiniciarJogo)
+
 //A variavel casa retorna uma NodeList,para adicionarmos um event listener em NodeList devemos usar o forEach()
 
 casa.forEach(function(celula) {
@@ -27,5 +30,15 @@ function marcarCasa(event) {
     else{
         JogadorAtual="X"
     }
+    turno.textContent=`Vez do Jogador: ${JogadorAtual}`
+}
+
+function ReiniciarJogo(){
+    casa.forEach(function(celula) {
+        celula.textContent = ""
+    });
+
+    JogadorAtual="X"
+
     turno.textContent=`Vez do Jogador: ${JogadorAtual}`
 }
