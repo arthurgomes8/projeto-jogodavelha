bc3897abf7111198c37bf68055778321ff5de46a
Arthur Gomes <arthurgcunha2@gmail.com>
2026-06-18 09:59:57 -0300

    Alternacia de turno dos players

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ let casa=document.querySelectorAll(".celula")
 //A variavel casa retorna uma NodeList,para adicionarmos um event listener em NodeList devemos usar o forEach()
 
 casa.forEach(function(celula) {
-    celula.addEventListener("click", marcarCasa);
+    celula.addEventListener("click", marcarCasa)
 });
 
 //Agora usamos a função marcarCasa que vai receber um relatorio de qual casinha foi clicada (event)
 
 function marcarCasa(event) {
-    let casinhaClicada = event.target;
-    casinhaClicada.textContent = JogadorAtual;
+    let casinhaClicada = event.target
+    casinhaClicada.textContent = JogadorAtual
+    //Alternar entre X e O para pintar as casas
+    if (JogadorAtual=="X"){
+        JogadorAtual="O"
+    }
+    else{
+        JogadorAtual="X"
+    }
+    turno.textContent=`Vez do Jogador: ${JogadorAtual}`
 }
